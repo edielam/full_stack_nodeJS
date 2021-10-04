fe2ae7fe10084867341bd08d9b306a1057e91f5e
edielam <edguard738@gmail.com>
2021-10-04 11:23:31 +0000

    keyless car v1.0

---
+++ keyless car/exx.js
@@ -0,0 +1,8 @@
+var ans = prompt("Enter your age");
+if (ans > 18) {
+    alert ("Powering On. Enjoy the ride!");
+} else if (ans == 18) {
+    alert ("Congratulations on your first year of driving. Enjoy the ride!");
+} else {
+    alert ("Sorry, you are too young to drive this car. Powering off");
+}
