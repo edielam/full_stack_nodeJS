0b25cb83ab4598edc91f1b30e98f4332cc07a56e
edielam <edguard738@gmail.com>
2021-10-04 12:12:07 +0000

    keyless car v2.0

--- keyless car/exx.js
+++ keyless car/exx.js
@@ -1,8 +1,22 @@
-var ans = prompt("Enter your age");
-if (ans > 18) {
-    alert ("Powering On. Enjoy the ride!");
-} else if (ans == 18) {
-    alert ("Congratulations on your first year of driving. Enjoy the ride!");
-} else {
-    alert ("Sorry, you are too young to drive this car. Powering off");
+/* function declaration */
+function checkDriverAge(ans) {
+    if (Number(ans) > 18) {
+        return "Powering On. Enjoy the ride!";
+    } else if (Number(ans) === 18) {
+        return "Congratulations on your first year of driving. Enjoy the ride!";
+    } else {
+        return "Sorry, you are too young to drive this car. Powering off";
+    }
+}
+
+/* function expression */
+var checkDriverAge2 = function() {
+    var age = prompt("Enter your age");
+    if (Number(age) > 18) {
+        alert ("Powering On. Enjoy the ride!");
+    } else if (Number(age) === 18) {
+        alert ("Congratulations on your first year of driving. Enjoy the ride!");
+    } else {
+        alert ("Sorry, you are too young to drive this car. Powering off");
+    }
 }
